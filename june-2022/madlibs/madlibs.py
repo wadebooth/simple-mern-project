0504906dd25c63bc8964ca5793cb7a666121c7fd
wadebooth <wademgmt@gmail.com>
2022-06-13 00:24:08 -0400

    added inputs

--- june-2022/madlibs/madlibs.py
+++ june-2022/madlibs/madlibs.py
@@ -1,10 +1,15 @@
 #string concatenation (aka how to put strings together)
 #suppose we want to create a string that says "Listen to ____"
 
-artist = '' #some string variable
+# artist = 'Smino' #some string variable
 
-#a few ways to do this
+# #a few ways to do this
 
-print("Subscribe to " + artist)
-print("Subscribe to {}".format(artist))
-print(f"subscribe to {artist}")
+# print("Subscribe to " + artist)
+# print("Subscribe to {}".format(artist))
+# print(f"subscribe to {artist}")
+
+adj = input("Adjective: ")
+verb = input("Verb: ")
+
+madlib = f'Computer programming is so {adj}! It makes me so excited because I love to {verb}.'

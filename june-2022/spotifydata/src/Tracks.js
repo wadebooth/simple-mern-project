f01754ec97bbad2c725c6b040c8f3fcd8a65534b
wadebooth <wademgmt@gmail.com>
2022-06-21 21:29:30 -0400

    adding useEffect to call post into Spotify API

--- june-2022/spotifydata/src/Tracks.js
+++ june-2022/spotifydata/src/Tracks.js
@@ -4,5 +4,19 @@ import axios from 'axios'
 
 export const Tracks = () => {
   const [token, setToken] = useState('')
+
+  useEffect(() => {
+    const [token, setToken] = useState('')
+
+    useEffect(() => {
+      axios('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-xxx-form-urlencoded',
+          Authorization: 'Basic ' + (client id + ':' + client secret)
+        },
+      })
+    })
+  })
   return <div>Tracks</div>
 }

abc9e4de8882c3e04232b41ce6987f6e14475116
wadebooth <wademgmt@gmail.com>
2022-06-21 21:21:59 -0400

    addeed tracks component

---
+++ june-2022/spotifydata/src/Tracks.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react'
+import Plot from 'react-plotly.js'
+import axios from 'axios'
+
+export const Tracks = () => {
+  const [token, setToken] = useState('')
+  return <div>Tracks</div>
+}
